ac329502e70d398b9fac75a3b43685f2e52d5244
Andres <dandres159@gmail.com>
2023-04-24 09:07:43 +0200

    Commit del proyecto

---
+++ src/MarvelApiApp.jsx
@@ -0,0 +1,12 @@
+
+
+
+export const MarvelApiApp = () => {
+  return (
+    <>
+        <h1>MarvelApiApp</h1>
+        <div>MarvelApiApp</div> 
+    </>
+    
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,11 @@
 import React from 'react';
+import ReactDOM from 'react-dom/client';
 
+import './styles.css';
+import { MarvelApiApp } from './MarvelApiApp';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MarvelApiApp />
+  </React.StrictMode>,
+)
